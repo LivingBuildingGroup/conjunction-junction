d4adb2143d4e7550ffaba40bedeeac9d17f74935
bradcgarner <brad@bradgarner.com>
2020-10-30 15:20:23 -0400

    updated yyyy-mm-dd hh-mm

--- src/date-time.js
+++ src/date-time.js
@@ -448,7 +448,7 @@ const _convertTimestampToStringInner = (ts, option) => {
           'AM' ;
   if(f === 'date')        return `${y}-${m0}-${d0}`;
   if(f === 'yyyy-mm-dd')  return `${y}-${m0}-${d0}`;
-  if(f === 'yyyy-mm-dd-hh-mm')  return `${y}-${m0}-${d0}-${h0}-${min0}`;
+  if(f === 'yyyy-mm-dd-hh-mm')  return `${y}-${m0}-${d0} ${h0}:${min0}`;
   if(f === 'd T noz')     return `${y}-${m0}-${d0}T${h0}:${min0}:${seconds0}`;
   if(f === 'd t noz')     return `${y}-${m0}-${d0} ${h0}:${min0}:${seconds0}`;
   if(f === 'd t z')       return `${y}-${m0}-${d0} ${h0}:${min0}:${seconds0} ${offsetFormattedNoColon}`;
